09ec11ff1ec24edab05c7943fb98829b3ad9106f
muralisrini <muralisr@us.ibm.com>
2015-10-01 12:33:54 -0400

    formatting, package name and fix logger compile error

--- openchain/vmcontroller_test.go
+++ openchain/vmcontroller_test.go
@@ -1,16 +1,35 @@
-package vmcontroller
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package openchain
 
 import (
-    "archive/tar"
-    "bytes"
-    "compress/gzip"
-    "io"
-    "time"
-    "io/ioutil"
-    "strings"
-    "testing"
-    "golang.org/x/net/context"
-    "github.com/spf13/viper"
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"github.com/spf13/viper"
+	"golang.org/x/net/context"
+	"io"
+	"io/ioutil"
+	"strings"
+	"testing"
+	"time"
 )
 
 /**** not using actual files from file system for testing.... use these funcs if we want to do that
@@ -30,7 +49,7 @@ func getCodeChainBytes(pathtocodechain string) (io.Reader, error) {
 }
 
 func writeCodeChainTar(pathtocodechain string, tw *tar.Writer) error {
-	root_directory := pathtocodechain //use full path 
+	root_directory := pathtocodechain //use full path
 	fmt.Printf("tar %s start(%s)\n", root_directory, time.Now())
 
 	walkFn := func(path string, info os.FileInfo, err error) error {
@@ -38,7 +57,7 @@ func writeCodeChainTar(pathtocodechain string, tw *tar.Writer) error {
                 if info == nil {
 	             return errors.New(fmt.Sprintf("Error walking the path: %s", path))
                 }
-       
+
 		if info.Mode().IsDir() {
 			return nil
 		}
@@ -100,52 +119,52 @@ func getCodeChainBytesInMem() (io.Reader, error) {
 	tr.Write([]byte(dockerFileContents))
 	tr.Close()
 	gw.Close()
-        ioutil.WriteFile("/tmp/chaincode.tar", inputbuf.Bytes(), 0644)
+	ioutil.WriteFile("/tmp/chaincode.tar", inputbuf.Bytes(), 0644)
 	return inputbuf, nil
 }
 
-func TestBuildImage(t *testing.T) {
-    viper.SetEnvPrefix("openchain")
-    viper.AutomaticEnv()
-    replacer := strings.NewReplacer(".", "_")
-    viper.SetEnvKeyReplacer(replacer)
-    viper.SetConfigName("vm") // name of config file (without extension)
-    viper.AddConfigPath("./")          // path to look for the config file in
-    if err := viper.ReadInConfig(); err != nil {        // Find and read the config file
-        t.Fail()
-        t.Logf("Error reading config file: %s", err)
-        return
-    }
+func TestVMCBuildImage(t *testing.T) {
+	viper.SetEnvPrefix("openchain")
+	viper.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetConfigName("vm")                    // name of config file (without extension)
+	viper.AddConfigPath("./")                    // path to look for the config file in
+	if err := viper.ReadInConfig(); err != nil { // Find and read the config file
+		t.Fail()
+		t.Logf("Error reading config file: %s", err)
+		return
+	}
 
-    //this creates a singleton... Peer will call it once to initialize
-    vmc := NewVMController()
+	//this creates a singleton... Peer will call it once to initialize
+	vmc := NewVMController()
 
-    var ctxt context.Context = context.Background()
+	var ctxt context.Context = context.Background()
 
-    //get the tarball for codechain
-    tarRdr, err := getCodeChainBytesInMem()
-    if err != nil {
-       t.Fail()
-       t.Logf("Error reading tar file: %s", err)
-       return
-    }
+	//get the tarball for codechain
+	tarRdr, err := getCodeChainBytesInMem()
+	if err != nil {
+		t.Fail()
+		t.Logf("Error reading tar file: %s", err)
+		return
+	}
 
-    c := make(chan struct{})
+	c := make(chan struct{})
 
-    //creat a CreateImageReq obj and send it to VMController.Process
-    go func() {
-        defer close(c)
-        args  := make([]string, 1)
-        args[0] = "-i"
-        cir := CreateImageReq{id: "simple", reader: tarRdr, args: args}
-        _, err := vmc.Process(ctxt, "Docker", cir)
-        if err != nil {
-           t.Fail()
-           t.Logf("Error creating image: %s", err)
-           return
-        }
-    } ()
+	//creat a CreateImageReq obj and send it to VMController.Process
+	go func() {
+		defer close(c)
+		args := make([]string, 1)
+		args[0] = "-i"
+		cir := CreateImageReq{id: "simple", reader: tarRdr, args: args}
+		_, err := vmc.Process(ctxt, "Docker", cir)
+		if err != nil {
+			t.Fail()
+			t.Logf("Error creating image: %s", err)
+			return
+		}
+	}()
 
-   //wait for VMController to complete.
-   <-c
+	//wait for VMController to complete.
+	<-c
 }
