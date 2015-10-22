2f536a0a1a10fb62269087638cabecf305e90d6a
Kostas Christidis <kostas@christidis.io>
2015-10-22 09:57:35 -0400

    Add leader FSM, update PBFT message definition

--- openchain/consensus/pbft/messages.pb.go
+++ openchain/consensus/pbft/messages.pb.go
@@ -9,52 +9,23 @@ It is generated from these files:
 	messages.proto
 
 It has these top-level messages:
+	PBFT
+	PBFTarray
 	Request
 	Phase
 	RequestHashes
 	Requests
-	PBFT
 */
 package pbft
 
 import proto "github.com/golang/protobuf/proto"
-
-// discarding unused import google_protobuf "google/protobuf"
+import fmt "fmt"
+import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
-
-type Phase_Type int32
-
-const (
-	Phase_UNDEFINED      Phase_Type = 0
-	Phase_PRE_PREPARE    Phase_Type = 1
-	Phase_PREPARE        Phase_Type = 2
-	Phase_COMMIT         Phase_Type = 3
-	Phase_PREPARE_RESULT Phase_Type = 4
-	Phase_COMMIT_RESULT  Phase_Type = 5
-)
-
-var Phase_Type_name = map[int32]string{
-	0: "UNDEFINED",
-	1: "PRE_PREPARE",
-	2: "PREPARE",
-	3: "COMMIT",
-	4: "PREPARE_RESULT",
-	5: "COMMIT_RESULT",
-}
-var Phase_Type_value = map[string]int32{
-	"UNDEFINED":      0,
-	"PRE_PREPARE":    1,
-	"PREPARE":        2,
-	"COMMIT":         3,
-	"PREPARE_RESULT": 4,
-	"COMMIT_RESULT":  5,
-}
-
-func (x Phase_Type) String() string {
-	return proto.EnumName(Phase_Type_name, int32(x))
-}
+var _ = fmt.Errorf
+var _ = math.Inf
 
 type PBFT_Type int32
 
@@ -89,6 +60,65 @@ var PBFT_Type_value = map[string]int32{
 
 func (x PBFT_Type) String() string {
 	return proto.EnumName(PBFT_Type_name, int32(x))
+}
+
+type Phase_Type int32
+
+const (
+	Phase_UNDEFINED      Phase_Type = 0
+	Phase_PRE_PREPARE    Phase_Type = 1
+	Phase_PREPARE        Phase_Type = 2
+	Phase_COMMIT         Phase_Type = 3
+	Phase_PREPARE_RESULT Phase_Type = 4
+	Phase_COMMIT_RESULT  Phase_Type = 5
+)
+
+var Phase_Type_name = map[int32]string{
+	0: "UNDEFINED",
+	1: "PRE_PREPARE",
+	2: "PREPARE",
+	3: "COMMIT",
+	4: "PREPARE_RESULT",
+	5: "COMMIT_RESULT",
+}
+var Phase_Type_value = map[string]int32{
+	"UNDEFINED":      0,
+	"PRE_PREPARE":    1,
+	"PREPARE":        2,
+	"COMMIT":         3,
+	"PREPARE_RESULT": 4,
+	"COMMIT_RESULT":  5,
+}
+
+func (x Phase_Type) String() string {
+	return proto.EnumName(Phase_Type_name, int32(x))
+}
+
+// Common message type.
+type PBFT struct {
+	Type    PBFT_Type `protobuf:"varint,1,opt,name=type,enum=pbft.PBFT_Type" json:"type,omitempty"`
+	Id      string    `protobuf:"bytes,2,opt,name=id" json:"id,omitempty"`
+	Payload []byte    `protobuf:"bytes,3,opt,name=payload,proto3" json:"payload,omitempty"`
+}
+
+func (m *PBFT) Reset()         { *m = PBFT{} }
+func (m *PBFT) String() string { return proto.CompactTextString(m) }
+func (*PBFT) ProtoMessage()    {}
+
+// Used for an array of requests.
+type PBFTarray struct {
+	Requests []*PBFT `protobuf:"bytes,1,rep,name=requests" json:"requests,omitempty"`
+}
+
+func (m *PBFTarray) Reset()         { *m = PBFTarray{} }
+func (m *PBFTarray) String() string { return proto.CompactTextString(m) }
+func (*PBFTarray) ProtoMessage()    {}
+
+func (m *PBFTarray) GetRequests() []*PBFT {
+	if m != nil {
+		return m.Requests
+	}
+	return nil
 }
 
 // Request is the message passed by the peer to the validator.
@@ -151,17 +181,7 @@ func (m *Requests) GetRequests() []*Requests {
 	return nil
 }
 
-type PBFT struct {
-	Type    PBFT_Type `protobuf:"varint,1,opt,name=type,enum=pbft.PBFT_Type" json:"type,omitempty"`
-	Id      string    `protobuf:"bytes,2,opt,name=id" json:"id,omitempty"`
-	Payload []byte    `protobuf:"bytes,3,opt,name=payload,proto3" json:"payload,omitempty"`
-}
-
-func (m *PBFT) Reset()         { *m = PBFT{} }
-func (m *PBFT) String() string { return proto.CompactTextString(m) }
-func (*PBFT) ProtoMessage()    {}
-
 func init() {
-	proto.RegisterEnum("pbft.Phase_Type", Phase_Type_name, Phase_Type_value)
 	proto.RegisterEnum("pbft.PBFT_Type", PBFT_Type_name, PBFT_Type_value)
+	proto.RegisterEnum("pbft.Phase_Type", Phase_Type_name, Phase_Type_value)
 }
