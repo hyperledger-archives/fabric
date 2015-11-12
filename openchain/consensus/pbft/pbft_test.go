c83777a99fd55f20baaf7fa1eb9b632b5574d8b5
jyellick <jason-devel-github@unaddressable.org>
2015-11-12 18:48:43 -0500

    De-lint, clean up some periods

--- openchain/consensus/pbft/pbft_test.go
+++ openchain/consensus/pbft/pbft_test.go
@@ -20,10 +20,9 @@ under the License.
 package pbft
 
 import (
-	"os"
-
 	"bytes"
 	gp "google/protobuf"
+	"os"
 	"testing"
 	"time"
 
@@ -35,20 +34,20 @@ import (
 
 func TestEnvOverride(t *testing.T) {
 
-	// For a key that exists.
+	// For a key that exists
 	key := "general.name"
 	// Env override name
 	envName := "OPENCHAIN_PBFT_GENERAL_NAME"
 	// Value to override default value with
 	overrideValue := "overide_test"
 
-	// Create new algorithm instance.
+	// Create new algorithm instance
 	helperInstance := helper.New()
 	instance := New(helperInstance)
 
 	// Test key.
 	if ok := instance.config.IsSet("general.name"); !ok {
-		t.Fatalf("Cannot test env override because \"%s\" does not seem to be set.", key)
+		t.Fatalf("Cannot test env override because \"%s\" does not seem to be set", key)
 	}
 
 	os.Setenv(envName, overrideValue)
@@ -58,56 +57,56 @@ func TestEnvOverride(t *testing.T) {
 	}()
 
 	if ok := instance.config.IsSet("general.name"); !ok {
-		t.Fatalf("Env override in place, and key \"%s\" is not set.", key)
+		t.Fatalf("Env override in place, and key \"%s\" is not set", key)
 	}
 
-	// Read key.
+	// Read key
 	configVal := instance.config.GetString("general.name")
 	if configVal != overrideValue {
-		t.Fatalf("Env override in place, expected key \"%s\" to be \"%s\" but instead got \"%s\".", key, overrideValue, configVal)
+		t.Fatalf("Env override in place, expected key \"%s\" to be \"%s\" but instead got \"%s\"", key, overrideValue, configVal)
 	}
 
 }
 
 func TestGetParam(t *testing.T) {
 
-	// Create new algorithm instance.
+	// Create new algorithm instance
 	helperInstance := helper.New()
 	instance := New(helperInstance)
 	helperInstance.SetConsenter(instance)
 
-	// For a key that exists.
+	// For a key that exists
 	key := "general.name"
-	// Expected value.
+	// Expected value
 	realVal := "pbft"
-	// Read key.
+	// Read key
 	testVal, err := instance.getParam(key)
-	// Error should be nil, since the key exists.
+	// Error should be nil, since the key exists
 	if err != nil {
 		t.Fatalf("Error when retrieving value for existing key %s: %s", key, err)
 	}
-	// Values should match.
+	// Values should match
 	if testVal != realVal {
-		t.Fatalf("Expected value %s for key %s, got %s instead.", realVal, key, testVal)
+		t.Fatalf("Expected value %s for key %s, got %s instead", realVal, key, testVal)
 	}
 
-	// Read key.
+	// Read key
 	key = "non.existing.key"
 	_, err = instance.getParam(key)
-	// Error should not be nil, since the key does not exist.
+	// Error should not be nil, since the key does not exist
 	if err == nil {
-		t.Fatal("Expected error since retrieving value for non-existing key, got nil instead.")
+		t.Fatal("Expected error since retrieving value for non-existing key, got nil instead")
 	}
 }
 
 func TestLeader(t *testing.T) {
 
-	// Create new algorithm instance.
+	// Create new algorithm instance
 	helperInstance := helper.New()
 	instance := New(helperInstance)
 	helperInstance.SetConsenter(instance)
 
-	// Do not access through `helperInstance.consenter.`
+	// Do not access through `helperInstance.consenter`
 	var ans bool
 	ans = instance.setLeader(true)
 	if !ans {
@@ -121,15 +120,15 @@ func TestLeader(t *testing.T) {
 
 func TestRecvMsg(t *testing.T) {
 
-	// Create new algorithm instance.
+	// Create new algorithm instance
 	helperInstance := helper.New()
 	instance := New(helperInstance)
 	helperInstance.SetConsenter(instance)
 
-	// Do not access through `helperInstance.consenter.`
+	// Do not access through `helperInstance.consenter`
 	var err error
 
-	// Create a message of type: `OpenchainMessage_REQUEST`.
+	// Create a message of type: `OpenchainMessage_REQUEST`
 	txTime := &gp.Timestamp{Seconds: time.Now().Unix(), Nanos: 0}
 	tx := &pb.Transaction{Type: pb.Transaction_CHAINLET_NEW, Timestamp: txTime}
 	txBlock := &pb.TransactionBlock{Transactions: []*pb.Transaction{tx}}
@@ -146,7 +145,7 @@ func TestRecvMsg(t *testing.T) {
 		t.Fatalf("Failed to handle OpenchainMessage:%s message: %s", msg.Type, err)
 	}
 
-	// Create a message of type: `OpenchainMessage_CONSENSUS`.
+	// Create a message of type: `OpenchainMessage_CONSENSUS`
 	msg.Type = pb.OpenchainMessage_CONSENSUS
 	nestedMsg := &Unpack{
 		Type:    Unpack_PREPARE,
@@ -165,23 +164,23 @@ func TestRecvMsg(t *testing.T) {
 
 func TestStoreRetrieve(t *testing.T) {
 
-	// Create new algorithm instance.
+	// Create new algorithm instance
 	helperInstance := helper.New()
 	instance := New(helperInstance)
 	helperInstance.SetConsenter(instance)
 
-	// Create a `REQUEST` message.
-	reqMsg := &Request2{Payload: []byte("helloworld")}
-	// Marshal it.
+	// Create a `REQUEST` message
+	reqMsg := &Request2{Payload: []byte("hello world")}
+	// Marshal it
 	reqMsgPacked, err := proto.Marshal(reqMsg)
 	if err != nil {
-		t.Fatalf("Error marshalling REQUEST message.")
+		t.Fatalf("Error marshalling REQUEST message")
 	}
 
-	// Get its hash.
+	// Get its hash
 	digest := hashMsg(reqMsgPacked)
 
-	// Store it.
+	// Store it
 	count := instance.storeRequest(digest, reqMsg)
 
 	if count != 1 {
@@ -192,16 +191,16 @@ func TestStoreRetrieve(t *testing.T) {
 
 	newMsg, err := instance.retrieveRequest(digest)
 	if err != nil {
-		t.Fatalf("Error retrieving REQUEST message from map.")
+		t.Fatalf("Error retrieving REQUEST message from map")
 	}
-	// Marshal it.
+	// Marshal it
 	newMsgPacked, err := proto.Marshal(newMsg)
 	if err != nil {
-		t.Fatalf("Error marshalling retrieved REQUEST message.")
+		t.Fatalf("Error marshalling retrieved REQUEST message")
 	}
 
 	// Are the two messages the same?
 	if !bytes.Equal(reqMsgPacked, newMsgPacked) {
-		t.Logf("Retrieved REQUEST message is not identical to original one.")
+		t.Logf("Retrieved REQUEST message is not identical to original one")
 	}
 }
