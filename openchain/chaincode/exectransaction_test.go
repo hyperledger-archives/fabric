bf04c17cfca69c22fdc78e48f53322938060539a
muralisrini <muralisr@us.ibm.com>
2015-11-13 17:27:37 -0500

    test changes #1

--- openchain/chaincode/exectransaction_test.go
+++ openchain/chaincode/exectransaction_test.go
@@ -89,6 +89,11 @@ func invoke(ctx context.Context, spec *pb.ChainletSpec) ([]byte, error) {
 	return Execute(ctx, GetChain(DEFAULTCHAIN), transaction)
 }
 
+func closeListenerAndSleep(l net.Listener) {
+	l.Close()
+	time.Sleep(2*time.Second)
+}
+
 func TestExecuteDeployTransaction(t *testing.T) {
 	var opts []grpc.ServerOption
 	if viper.GetBool("peer.tls.enabled") {
@@ -100,12 +105,18 @@ func TestExecuteDeployTransaction(t *testing.T) {
 	}
 	grpcServer := grpc.NewServer(opts...)
 
-	lis, err := net.Listen("tcp", viper.GetString("peer.address"))
+	//lis, err := net.Listen("tcp", viper.GetString("peer.address"))
+
+	//use a different address than what we usually use for "peer"
+	//we override the PEERADDRESS set in chaincode_support.go
+        PEERADDRESS = "0.0.0.0:40303"
+	lis, err := net.Listen("tcp", PEERADDRESS)
 	if err != nil {
 		t.Fail()
 		t.Logf("Error starting peer listener %s", err)
 		return
 	}
+
 
 	pb.RegisterChainletSupportServer(grpcServer, NewChainletSupport())
 	
@@ -124,10 +135,12 @@ func TestExecuteDeployTransaction(t *testing.T) {
 	chaincodeID,_ := getChaincodeID(&pb.ChainletID{Url: url, Version: version})
 	_,err = deploy(ctxt, spec)
 	if err != nil {
+		closeListenerAndSleep(lis)
 		t.Fail()
 		t.Logf("Error deploying <%s>: %s", chaincodeID, err)
 		return
 	}
+	closeListenerAndSleep(lis)
 }
 
 func TestExecuteInvokeTransaction(t *testing.T) {
@@ -141,7 +154,11 @@ func TestExecuteInvokeTransaction(t *testing.T) {
 	}
 	grpcServer := grpc.NewServer(opts...)
 
-	lis, err := net.Listen("tcp", viper.GetString("peer.address"))
+	//use a different address than what we usually use for "peer"
+	//we override the PEERADDRESS set in chaincode_support.go
+        PEERADDRESS = "0.0.0.0:40303"
+
+	lis, err := net.Listen("tcp", PEERADDRESS)
 	if err != nil {
 		t.Fail()
 		t.Logf("Error starting peer listener %s", err)
@@ -167,19 +184,21 @@ func TestExecuteInvokeTransaction(t *testing.T) {
 	if err != nil {
 		t.Fail()
 		t.Logf("Error deploying <%s>: %s", chaincodeID, err)
+		closeListenerAndSleep(lis)
 		return
 	}
 
 	time.Sleep(time.Second)
+
 	fmt.Printf("Going to invoke")
 	f = "invoke"
 	args = []string{ "a", "b", "10" }
-        spec = &pb.ChainletSpec { Type: 1, ChainletID: &pb.ChainletID{ Url: url, Version: version }, CtorMsg: &pb.ChaincodeInput{ Function : f, Args: args } }
+       	spec = &pb.ChainletSpec { Type: 1, ChainletID: &pb.ChainletID{ Url: url, Version: version }, CtorMsg: &pb.ChaincodeInput{ Function : f, Args: args } }
 	_,err = invoke(ctxt, spec)
 	if err != nil {
-		time.Sleep(10 * time.Second)
 		t.Fail()
 		t.Logf("Error invoking <%s>: %s", chaincodeID, err)
+		closeListenerAndSleep(lis)
 		return
 	}
 
@@ -188,6 +207,7 @@ func TestExecuteInvokeTransaction(t *testing.T) {
 	if ledgerErr != nil {
 		t.Fail()
 		t.Logf("Error checking ledger for <%s>: %s", chaincodeID, ledgerErr)
+		closeListenerAndSleep(lis)
 		return
 	}
 
@@ -197,17 +217,20 @@ func TestExecuteInvokeTransaction(t *testing.T) {
 	if resErr != nil {
 		t.Fail()
 		t.Logf("Error retrieving state from ledger for <%s>: %s", chaincodeID, resErr)
+		closeListenerAndSleep(lis)
 		return
 	} 
 	Aval, resErr = strconv.Atoi(string(resbytes))
 	if resErr != nil {
 		t.Fail()
 		t.Logf("Error retrieving state from ledger for <%s>: %s", chaincodeID, resErr)
+		closeListenerAndSleep(lis)
 		return
 	}
 	if Aval != 90 {
 		t.Fail()
 		t.Logf("Incorrect result. Aval is wrong for <%s>", chaincodeID)
+		closeListenerAndSleep(lis)
 		return
 	}
 
@@ -215,22 +238,27 @@ func TestExecuteInvokeTransaction(t *testing.T) {
 	if resErr != nil {
 		t.Fail()
 		t.Logf("Error retrieving state from ledger for <%s>: %s", chaincodeID, resErr)
+		closeListenerAndSleep(lis)
 		return
 	} 
 	Bval, resErr = strconv.Atoi(string(resbytes))
 	if resErr != nil {
 		t.Fail()
 		t.Logf("Error retrieving state from ledger for <%s>: %s", chaincodeID, resErr)
+		closeListenerAndSleep(lis)
 		return
 	}
 	if Bval != 210 {
 		t.Fail()
 		t.Logf("Incorrect result. Bval is wrong for <%s>", chaincodeID)
+		closeListenerAndSleep(lis)
 		return
 	}
 	fmt.Printf("Aval = %d, Bval = %d\n",Aval, Bval)
 	fmt.Printf("Test passed")
 	t.Logf("Test passed")
+
+	closeListenerAndSleep(lis)
 }
 
 func TestMain(m *testing.M) {
