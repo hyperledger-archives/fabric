74af9ca55487e96ab77850b69d608e439ed16e09
manish <manishsethi@in.ibm.com>
2015-11-18 19:58:57 +0530

    Added support for rolling back a tx if chaincode execution fails

--- openchain/chaincode/exectransaction.go
+++ openchain/chaincode/exectransaction.go
@@ -32,23 +32,33 @@ import (
 )
 
 //Execute - execute transaction or a query
-func  Execute(ctxt context.Context, chain *ChainletSupport, t *pb.Transaction) ([]byte, error) {
+func Execute(ctxt context.Context, chain *ChainletSupport, t *pb.Transaction) ([]byte, error) {
 	var err error
+
+	// get a handle to ledger to mark the begin/finish of a tx
+	ledger, ledgerErr := ledger.GetLedger()
+	if ledgerErr != nil {
+		return nil, fmt.Errorf("Failed to get handle to ledger (%s)", ledgerErr)
+	}
+
 	if t.Type == pb.Transaction_CHAINLET_NEW {
-		_,err := chain.DeployChaincode(ctxt, t)
+		_, err := chain.DeployChaincode(ctxt, t)
 		if err != nil {
 			return nil, fmt.Errorf("Failed to deploy chaincode spec(%s)", err)
 		}
 
 		//launch and wait for ready
-		_,_,err = chain.LaunchChaincode(ctxt, t)
+		markTxBegin(ledger, t)
+		_, _, err = chain.LaunchChaincode(ctxt, t)
 		if err != nil {
 			//TODO rollback transaction as init might have set state
+			markTxFinish(ledger, t, false)
 			return nil, fmt.Errorf("Failed to launch chaincode spec(%s)", err)
 		}
+		markTxFinish(ledger, t, true)
 	} else if t.Type == pb.Transaction_CHAINLET_EXECUTE || t.Type == pb.Transaction_CHAINLET_QUERY {
 		//will launch if necessary (and wait for ready)
-		cID,cMsg,err := chain.LaunchChaincode(ctxt, t)
+		cID, cMsg, err := chain.LaunchChaincode(ctxt, t)
 		if err != nil {
 			return nil, fmt.Errorf("Failed to launch chaincode spec(%s)", err)
 		}
@@ -56,63 +66,68 @@ func  Execute(ctxt context.Context, chain *ChainletSupport, t *pb.Transaction) (
 		//this should work because it worked above...
 		chaincode, _ := getChaincodeID(cID)
 
-		if(err != nil){
+		if err != nil {
 			return nil, fmt.Errorf("Failed to stablish stream to container %s", chaincode)
 		}
-	
-		// TODO: Need to comment next line and uncomment call to getTimeout, when transaction blocks are being created	
+
+		// TODO: Need to comment next line and uncomment call to getTimeout, when transaction blocks are being created
 		timeout := time.Duration(30000) * time.Millisecond
 		//timeout, err := getTimeout(cID)
-		
-		if(err != nil){
+
+		if err != nil {
 			return nil, fmt.Errorf("Failed to retrieve chaincode spec(%s)", err)
 		}
-		
+
 		var ccMsg *pb.ChaincodeMessage
 		if t.Type == pb.Transaction_CHAINLET_EXECUTE {
-			ccMsg,err = createTransactionMessage(t.Uuid, cMsg)
+			ccMsg, err = createTransactionMessage(t.Uuid, cMsg)
 			if err != nil {
 				return nil, fmt.Errorf("Failed to transaction message(%s)", err)
 			}
 		} else {
-			ccMsg,err = createQueryMessage(t.Uuid, cMsg)
+			ccMsg, err = createQueryMessage(t.Uuid, cMsg)
 			if err != nil {
 				return nil, fmt.Errorf("Failed to query message(%s)", err)
 			}
 		}
 
-		resp,err := chain.Execute(ctxt, chaincode, ccMsg, timeout)
+		markTxBegin(ledger, t)
+		resp, err := chain.Execute(ctxt, chaincode, ccMsg, timeout)
 		if err != nil {
 			//TODO rollback transaction....
+			markTxFinish(ledger, t, false)
 			return nil, fmt.Errorf("Failed to execute transaction(%s)", err)
 		} else if resp == nil {
 			//TODO rollback transaction....
+			markTxFinish(ledger, t, false)
 			return nil, fmt.Errorf("Failed to receive a response for (%s)", t.Uuid)
 		} else {
-			if resp.Type == pb.ChaincodeMessage_COMPLETED || resp.Type == pb.ChaincodeMessage_QUERY_COMPLETED || 
-			   resp.Type == pb.ChaincodeMessage_ERROR || resp.Type == pb.ChaincodeMessage_QUERY_ERROR {
-				return 	resp.Payload, nil
+			if resp.Type == pb.ChaincodeMessage_COMPLETED || resp.Type == pb.ChaincodeMessage_QUERY_COMPLETED ||
+				resp.Type == pb.ChaincodeMessage_ERROR || resp.Type == pb.ChaincodeMessage_QUERY_ERROR {
+				markTxFinish(ledger, t, true)
+				return resp.Payload, nil
 			}
+			markTxFinish(ledger, t, false)
 			return resp.Payload, fmt.Errorf("receive a response for (%s) but in invalid state(%d)", t.Uuid, resp.Type)
 		}
 
 	} else {
 		err = fmt.Errorf("Invalid transaction type %s", t.Type.String())
 	}
-	return  nil,err
+	return nil, err
 }
 
 //ExecuteTransactions - will execute transactions on the array one by one
 //will return an array of errors one for each transaction. If the execution
 //succeeded, array element will be nil. returns state hash
-func  ExecuteTransactions(ctxt context.Context, cname ChainName, xacts []*pb.Transaction) ([]byte, []error) {
+func ExecuteTransactions(ctxt context.Context, cname ChainName, xacts []*pb.Transaction) ([]byte, []error) {
 	var chain = GetChain(cname)
 	if chain == nil {
 		panic(fmt.Sprintf("[ExecuteTransactions]Chain %s not found\n", cname))
 	}
 	errs := make([]error, len(xacts)+1)
 	for i, t := range xacts {
-		_,errs[i] = Execute(ctxt,chain,t)
+		_, errs[i] = Execute(ctxt, chain, t)
 	}
 	ledger, hasherr := ledger.GetLedger()
 	var statehash []byte
@@ -141,6 +156,20 @@ func getTimeout(cID *pb.ChainletID) (time.Duration, error) {
 			}
 		}
 	}
-	
+
 	return -1, errFailedToGetChainCodeSpecForTransaction
+}
+
+func markTxBegin(ledger *ledger.Ledger, t *pb.Transaction) {
+	if t.Type == pb.Transaction_CHAINLET_QUERY {
+		return
+	}
+	ledger.TxBegin(t.Uuid)
+}
+
+func markTxFinish(ledger *ledger.Ledger, t *pb.Transaction, successful bool) {
+	if t.Type == pb.Transaction_CHAINLET_QUERY {
+		return
+	}
+	ledger.TxFinished(t.Uuid, successful)
 }
