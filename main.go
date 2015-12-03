bb3151b6845aa092340468cf59d6eb0fac2c2f6d
srderson <sranderson@gmail.com>
2015-12-02 20:36:50 -0500

    Call MakeGenesis from main. Closes #119

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ import (
 	"github.com/openblockchain/obc-peer/openchain"
 	"github.com/openblockchain/obc-peer/openchain/chaincode"
 	"github.com/openblockchain/obc-peer/openchain/consensus/helper"
+	"github.com/openblockchain/obc-peer/openchain/ledger/genesis"
 	"github.com/openblockchain/obc-peer/openchain/peer"
 	"github.com/openblockchain/obc-peer/openchain/rest"
 	pb "github.com/openblockchain/obc-peer/protos"
@@ -200,7 +201,6 @@ func main() {
 	viper.BindPFlag("peer_gomaxprocs", flags.Lookup("peer-gomaxprocs"))
 	viper.BindPFlag("peer_discovery_enabled", flags.Lookup("peer-discovery-enabled"))
 
-
 	// Now set the configuration file.
 	viper.SetConfigName(cmdRoot) // Name of config file (without extension)
 	viper.AddConfigPath("./")    // Path to look for the config file in
@@ -320,7 +320,24 @@ func serve(args []string) error {
 	logger.Info("Starting peer with id=%s, network id=%s, address=%s, discovery.rootnode=%s, validator=%v",
 		peerEndpoint.ID, viper.GetString("peer.networkId"),
 		peerEndpoint.Address, rootNode, viper.GetBool("peer.validator.enabled"))
-	grpcServer.Serve(lis)
+
+	// Start the grpc server. Done in a goroutine so we can deploy the
+	// genesis block if needed.
+	serve := make(chan bool)
+	go func() {
+		grpcServer.Serve(lis)
+		serve <- true
+	}()
+
+	// Deploy the geneis block if needed.
+	makeGeneisError := genesis.MakeGenesis()
+	if makeGeneisError != nil {
+		return makeGeneisError
+	}
+
+	// Block until grpc server exits
+	<-serve
+
 	return nil
 }
 
