5d9a32e621238d6a695aaeb19e35824561b6ffbf
manish <manishsethi@in.ibm.com>
2015-12-14 11:46:50 +0530

    Statesnapshot for trie based state implementation

--- openchain/ledger/statemgmt/trie/snapshot_iterator_test.go
+++ openchain/ledger/statemgmt/trie/snapshot_iterator_test.go
@@ -42,30 +42,46 @@ func TestStateSnapshotIterator(t *testing.T) {
 	stateTrie.PrepareWorkingSet(stateDelta)
 	stateTrieTestWrapper.PersistChangesAndResetInMemoryChanges()
 	//check that the key is persisted
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID1", "key1"), []byte("value1"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID2", "key2"), []byte("value2"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID3", "key3"), []byte("value3"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID4", "key4"), []byte("value4"))
 	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID5", "key5"), []byte("value5"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID6", "key6"), []byte("value6"))
 
 	// take db snapeshot
 	dbSnapshot := db.GetDBHandle().GetSnapshot()
 
-	// delete keys
-	stateDelta.Delete("chaincodeID1", "key1")
-	stateDelta.Delete("chaincodeID2", "key2")
-	stateDelta.Delete("chaincodeID3", "key3")
-	stateDelta.Delete("chaincodeID4", "key4")
-	stateDelta.Delete("chaincodeID5", "key5")
-	stateDelta.Delete("chaincodeID6", "key6")
-	stateTrie.PrepareWorkingSet(stateDelta)
+	stateDelta1 := statemgmt.NewStateDelta()
+	// delete a few keys
+	stateDelta1.Delete("chaincodeID1", "key1")
+	stateDelta1.Delete("chaincodeID3", "key3")
+	stateDelta1.Delete("chaincodeID4", "key4")
+	stateDelta1.Delete("chaincodeID6", "key6")
+
+	// update remaining keys
+	stateDelta1.Set("chaincodeID2", "key2", []byte("value2_new"))
+	stateDelta1.Set("chaincodeID5", "key5", []byte("value5_new"))
+
+	stateTrie.PrepareWorkingSet(stateDelta1)
 	stateTrieTestWrapper.PersistChangesAndResetInMemoryChanges()
-	//check that the key is deleted
-	testutil.AssertNil(t, stateTrieTestWrapper.Get("chaincodeID5", "key5"))
+	//check that the keys are updated
+	testutil.AssertNil(t, stateTrieTestWrapper.Get("chaincodeID1", "key1"))
+	testutil.AssertNil(t, stateTrieTestWrapper.Get("chaincodeID3", "key3"))
+	testutil.AssertNil(t, stateTrieTestWrapper.Get("chaincodeID4", "key4"))
+	testutil.AssertNil(t, stateTrieTestWrapper.Get("chaincodeID6", "key6"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID2", "key2"), []byte("value2_new"))
+	testutil.AssertEquals(t, stateTrieTestWrapper.Get("chaincodeID5", "key5"), []byte("value5_new"))
 
 	itr, err := newStateSnapshotIterator(dbSnapshot)
 	testutil.AssertNoError(t, err, "Error while getting state snapeshot iterator")
-	numKeys := 0
+
+	stateDeltaFromSnapshot := statemgmt.NewStateDelta()
 	for itr.Next() {
-		key, value := itr.GetRawKeyValue()
-		t.Logf("key=[%s], value=[%s]", string(key), string(value))
-		numKeys++
+		keyBytes, valueBytes := itr.GetRawKeyValue()
+		t.Logf("key=[%s], value=[%s]", string(keyBytes), string(valueBytes))
+		chaincodeID, key := statemgmt.DecodeCompositeKey(keyBytes)
+		stateDeltaFromSnapshot.Set(chaincodeID, key, valueBytes)
 	}
-	testutil.AssertEquals(t, numKeys, 6)
+	testutil.AssertEquals(t, stateDelta, stateDeltaFromSnapshot)
 }
