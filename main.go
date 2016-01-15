369a16ffa50839b4e91de3becd5874eea4ae1eb7
Anna Derbakova <adderbak@us.ibm.com>
2016-01-15 18:35:45 -0500

    Updating transaction privacy check
    Updating transaction privacy check to be done only when the security is
    already enabled.
    
    Signed-off-by: Anna D Derbakova <adderbak@us.ibm.com>

--- main.go
+++ main.go
@@ -605,6 +605,11 @@ func chaincodeDeploy(cmd *cobra.Command, args []string) {
 
 			// Add the login token to the chaincodeSpec
 			spec.SecureContext = string(token)
+
+			// If privacy is enabled, mark chaincode as confidential
+			if viper.GetBool("security.privacy") {
+				spec.ConfidentialityLevel = pb.ConfidentialityLevel_CONFIDENTIAL
+			}
 		} else {
 			// Check if the token is not there and fail
 			if os.IsNotExist(err) {
@@ -614,11 +619,6 @@ func chaincodeDeploy(cmd *cobra.Command, args []string) {
 			// Unexpected error
 			panic(fmt.Errorf("Fatal error when checking for client login token: %s\n", err))
 		}
-	}
-
-	// If privacy is enabled, mark chaincode as confidential
-	if viper.GetBool("security.privacy") {
-		spec.ConfidentialityLevel = pb.ConfidentialityLevel_CONFIDENTIAL
 	}
 
 	chaincodeDeploymentSpec, err := devopsClient.Deploy(context.Background(), spec)
@@ -689,6 +689,11 @@ func chaincodeInvokeOrQuery(cmd *cobra.Command, args []string, invoke bool) {
 
 			// Add the login token to the chaincodeSpec
 			spec.SecureContext = string(token)
+
+			// If privacy is enabled, mark chaincode as confidential
+			if viper.GetBool("security.privacy") {
+				spec.ConfidentialityLevel = pb.ConfidentialityLevel_CONFIDENTIAL
+			}
 		} else {
 			// Check if the token is not there and fail
 			if os.IsNotExist(err) {
@@ -698,11 +703,6 @@ func chaincodeInvokeOrQuery(cmd *cobra.Command, args []string, invoke bool) {
 			// Unexpected error
 			panic(fmt.Errorf("Fatal error when checking for client login token: %s\n", err))
 		}
-	}
-
-	// If privacy is enabled, mark chaincode as confidential
-	if viper.GetBool("security.privacy") {
-		spec.ConfidentialityLevel = pb.ConfidentialityLevel_CONFIDENTIAL
 	}
 
 	// Build the ChaincodeInvocationSpec message
