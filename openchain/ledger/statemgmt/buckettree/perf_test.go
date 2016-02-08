adad8eb372f0103843657911cbaa523e1202d1a6
manish <manishsethi@in.ibm.com>
2016-02-08 10:59:46 +0530

    Added benchmark test for bucket hash

---
+++ openchain/ledger/statemgmt/buckettree/perf_test.go
@@ -0,0 +1,62 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package buckettree
+
+import (
+	"flag"
+	"github.com/op/go-logging"
+	"github.com/openblockchain/obc-peer/openchain/ledger/statemgmt"
+	"github.com/openblockchain/obc-peer/openchain/ledger/testutil"
+	"testing"
+)
+
+func BenchmarkStateHash(b *testing.B) {
+	if b.N != 1 {
+		b.Fatal(`This benchmark should be called with param -benchtime=0 so that it runs only once.
+			Otherwise, each time this addes data to the DB which affects the measurement.`)
+	}
+
+	b.Logf("testParams:%q", testParams)
+	flags := flag.NewFlagSet("testParams", flag.ExitOnError)
+	numBuckets := flags.Int("NumBuckets", 10009, "Number of buckets")
+	maxGroupingAtEachLevel := flags.Int("MaxGroupingAtEachLevel", 10, "max grouping at each level")
+	chaincodeIDPrefix := flags.String("ChaincodeIDPrefix", "cID", "The chaincodeID used in the generated workload will be  ChaincodeIDPrefix_1, ChaincodeIDPrefix_2, and so on")
+	numChaincodes := flags.Int("NumChaincodes", 1, "Number of chaincodes to assume")
+	maxKeySuffix := flags.Int("MaxKeySuffix", 1, "the keys are appended with _1, _2,.. upto MaxKeySuffix")
+	numKeysToInsert := flags.Int("NumKeysToInsert", 1, "how many keys to insert in a single batch")
+	valueSize := flags.Int("ValueSize", 1000, "size of the value")
+	debugMsgsOn := flags.Bool("DebugOn", false, "Trun on/off debug messages during benchmarking")
+	flags.Parse(testParams)
+
+	b.Logf(`Running test with params:
+		numbBuckets=%d, maxGroupingAtEachLevel=%d, chaincodeIDPrefix=%s, numChaincodes=%d, maxKeySuffix=%d, numKeysToInsert=%d, valueSize=%d, debugMsgs=%t`,
+		*numBuckets, *maxGroupingAtEachLevel, *chaincodeIDPrefix, *numChaincodes, *maxKeySuffix, *numKeysToInsert, *valueSize, *debugMsgsOn)
+
+	if !*debugMsgsOn {
+		testutil.SetLogLevel(logging.ERROR, "buckettree")
+		testutil.SetLogLevel(logging.ERROR, "db")
+	}
+
+	stateImplTestWrapper := newStateImplTestWrapperWithCustomConfig(b, *numBuckets, *maxGroupingAtEachLevel)
+	delta := statemgmt.ConstructRandomStateDelta(b, *chaincodeIDPrefix, *numChaincodes, *maxKeySuffix, *numKeysToInsert, *valueSize)
+	b.ResetTimer()
+	stateImplTestWrapper.prepareWorkingSet(delta)
+	stateImplTestWrapper.persistChangesAndResetInMemoryChanges()
+}
