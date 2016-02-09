b5138ce8dc5aa576dc7e0eaafe76f5c94589f2ec
srderson <sranderson@gmail.com>
2016-02-09 17:58:38 -0500

    Fix error in handling of error cases in CommitTxBatch. Fixes #632

--- openchain/ledger/ledger.go
+++ openchain/ledger/ledger.go
@@ -116,13 +116,10 @@ func (ledger *Ledger) CommitTxBatch(id interface{}, transactions []*protos.Trans
 		return err
 	}
 
-	success := true
-	defer ledger.resetForNextTxGroup(success)
-	defer ledger.blockchain.blockPersistenceStatus(success)
-
 	stateHash, err := ledger.state.GetHash()
 	if err != nil {
-		success = false
+		ledger.resetForNextTxGroup(false)
+		ledger.blockchain.blockPersistenceStatus(false)
 		return err
 	}
 
@@ -131,16 +128,22 @@ func (ledger *Ledger) CommitTxBatch(id interface{}, transactions []*protos.Trans
 	block.NonHashData = &protos.NonHashData{TransactionResults: transactionResults}
 	newBlockNumber, err := ledger.blockchain.addPersistenceChangesForNewBlock(context.TODO(), block, stateHash, writeBatch)
 	if err != nil {
-		success = false
+		ledger.resetForNextTxGroup(false)
+		ledger.blockchain.blockPersistenceStatus(false)
 		return err
 	}
 	ledger.state.AddChangesForPersistence(newBlockNumber, writeBatch)
 	opt := gorocksdb.NewDefaultWriteOptions()
 	dbErr := db.GetDBHandle().DB.Write(opt, writeBatch)
 	if dbErr != nil {
-		success = false
+		ledger.resetForNextTxGroup(false)
+		ledger.blockchain.blockPersistenceStatus(false)
 		return dbErr
 	}
+
+	ledger.resetForNextTxGroup(true)
+	ledger.blockchain.blockPersistenceStatus(true)
+
 	producer.Send(producer.CreateBlockEvent(block))
 	return nil
 }
