90e3af03dde3939bec23cb72aff624ed0c21ed38
Simon Schubert <sis@zurich.ibm.com>
2016-02-09 16:02:36 +0100

    pbft/test: increase viewchange timeout
    Signed-off-by: Simon Schubert <sis@zurich.ibm.com>

--- openchain/consensus/obcpbft/pbft-core_test.go
+++ openchain/consensus/obcpbft/pbft-core_test.go
@@ -531,7 +531,7 @@ func TestViewChangeWithStateTransfer(t *testing.T) {
 }
 
 func TestNewViewTimeout(t *testing.T) {
-	millisUntilTimeout := time.Duration(10)
+	millisUntilTimeout := time.Duration(100)
 
 	if testing.Short() {
 		t.Skip("Skipping timeout test")
