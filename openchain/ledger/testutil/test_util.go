0c2175e993f7dc8035c09c2931f66de70c87a454
manish <manishsethi@in.ibm.com>
2016-02-17 21:13:28 +0530

    minor changes for perf tests

--- openchain/ledger/testutil/test_util.go
+++ openchain/ledger/testutil/test_util.go
@@ -26,6 +26,7 @@ import (
 	"reflect"
 	"regexp"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/op/go-logging"
@@ -35,11 +36,13 @@ import (
 
 func SetupTestConfig() {
 	viper.AddConfigPath(".")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetConfigName("test")
 	err := viper.ReadInConfig()
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
+	viper.AutomaticEnv()
 
 	var formatter = logging.MustStringFormatter(
 		`%{color}%{time:15:04:05.000} [%{module}] %{shortfunc} [%{shortfile}] -> %{level:.4s} %{id:03x}%{color:reset} %{message}`,
