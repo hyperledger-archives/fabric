272980c11062ea0cb4a86ef72e0c1cb223d17346
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-03-09 18:25:58 -0500

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -4,7 +4,7 @@ echo " Pull value $1"
 if [ "$1" = "false" ]; then
 rm -rf $HOME/gopath/src/github.com/openblockchain/
 echo "deleted"
-cp -r $HOME/gopath/src/github.com/$GIT_USER $HOME/gopath/src/github.com/openblockchain
+cp -r $HOME/gopath/src/github.com/$USER_NAME $HOME/gopath/src/github.com/openblockchain
 echo "copied"
 fi
 echo "Completed"
