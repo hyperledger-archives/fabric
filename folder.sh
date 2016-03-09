321ac27e5ee2b864a3abab214208b61d35ed1629
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-03-09 11:40:08 -0500

    Update folder.sh

--- folder.sh
+++ folder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 rm -rf $HOME/gopath/src/github.com/openblockchain/
 echo "deleted"
 cp -r $HOME/gopath/src/github.com/rameshthoomu $HOME/gopath/src/github.com/openblockchain
