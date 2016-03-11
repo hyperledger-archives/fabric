f894696bc6f81ebdf438cc7610c7ca25c4f24792
rameshthoomu <rameshbabu.thoomu@gmail.com>
2016-03-11 16:46:00 -0500

    Update foldercopy.sh

--- foldercopy.sh
+++ foldercopy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo " Pull Request value is $1"
 if [ "$1" = "false" ] && [ "$2" != "openblockchain" ]; then
 rm -rf $HOME/gopath/src/github.com/openblockchain/
 echo "Deleted openblockchain folder"
@@ -9,7 +8,4 @@ elif [ "$2" != "openblockchain" ]; then
 echo "creatng mkdir"
 #mkdir -p $HOME/gopath/src/github.com/openblockchain/
 mv $HOME/gopath/src/github.com/$2 $HOME/gopath/src/github.com/openblockchain
-echo $HOME/gopath/src/github.com/openblockchain
-ls $HOME/gopath/src/github.com/openblockchain/obc-peer
-echo "Created and copied openblockchain folder"
 fi
