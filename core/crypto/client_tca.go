4695f6d9dcf317139a97b221e3e87a1296b0d81f
Andres Garagiola <andres@oc2535404202.ibm.com>
2016-04-08 19:33:05 -0300

    Adding read attributes AI

--- core/crypto/client_tca.go
+++ core/crypto/client_tca.go
@@ -25,6 +25,10 @@ import (
 	"bytes"
 	"crypto/ecdsa"
 	"crypto/hmac"
+	"encoding/asn1"
+
+	"strings"
+	"strconv"
 	"errors"
 	"fmt"
 	"github.com/golang/protobuf/proto"
@@ -493,4 +497,65 @@ func (client *clientImpl) callTCACreateCertificateSet(num int) ([]byte, [][]byte
 	}
 
 	return certSet.Certs.Key, certSet.Certs.Certs, nil
+}
+
+func (client *clientImpl) parseHeader(header string) (map[string]int, error) { 
+	tokens :=  strings.Split(header, "#")
+	answer := make(map[string]int)
+	for _, token := range tokens {
+		pair:= strings.Split(token, "->")
+		if len(pair) != 2 {
+			return nil, errors.New("Header attributes invalid format.")
+		}
+		key := pair[0]
+		valueStr := pair[1]
+		value, err := strconv.Atoi(valueStr)
+		if err != nil { 
+			return nil, err
+		}
+		answer[key] = value
+	}
+	return answer, nil
+	
+}
+// Read the attribute with name 'attributeName' from the der encoded x509.Certificate 'tcertder'.
+//
+func (client *clientImpl) ReadAttribute(attributeName string, tcertder []byte) ([]byte, error) {
+	tcert, err := utils.DERToX509Certificate(tcertder)
+	if err != nil {
+		client.debug("Failed parsing certificate [% x]: [%s].", tcertder, err)
+
+		return nil, err
+	}
+	
+	var header_raw []byte
+	if header_raw, err = utils.GetCriticalExtension(tcert, utils.TCertAttributesHeaders); err != nil {
+		client.error("Failed getting extension TCERT_ATTRIBUTES_HEADER [% x]: [%s].", tcertder, err)
+
+		return nil, err
+	}
+	
+	header_str := string(header_raw)	
+	var header map[string]int
+	header, err = client.parseHeader(header_str)
+	
+	if err != nil {
+		return nil, err
+	}
+	
+	position := header[attributeName]
+	
+	if position == 0 {
+		return nil, errors.New("Failed attribute doesn't exists in the TCert.")
+	}
+
+    oid := asn1.ObjectIdentifier{1, 2, 3, 4, 5, 6, 9 + position}
+    
+    var value []byte
+    if value, err = utils.GetCriticalExtension(tcert, oid); err != nil {
+		client.error("Failed getting extension Attribute Value [% x]: [%s].", tcertder, err)
+		return nil, err
+	}
+    
+    return value, nil
 }
