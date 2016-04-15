ff8e1d1369f913404d806f7f508d297b2174f649
Keith Smith <bksmith@us.ibm.com>
2016-04-15 14:17:25 -0400

    add README.md and rename Wallet to KeyValStore

--- sdk/node/test.js
+++ sdk/node/test.js
@@ -4,9 +4,8 @@
 
 var hlc = require(__dirname+'/hlc');
 
-var chainMgr = hlc.NewChainMgr();
-var chain = chainMgr.getChain("test1",true);
-chain.configureWallet({dir:"/tmp/wallet"});
+var chain = hlc.newChain("testChain");
+chain.configureKeyValStore({dir:"/tmp/keyValStore"});
 chain.setMemberServicesUrl("grpc://localhost:50051");
 // Get the web app administrator member which is set as the chain registrar
 // whose credentials are (or will be) used to authorize registering other web users.
@@ -14,7 +13,7 @@ chain.getMember("myWebAppAdmin",function(err,webAppAdmin) {
 	if (err) return console.log("failed to get webAppAdmin member");
 	// Assume the webAppAdmin has already been enrolled using the secret provided by member services administrator
 	// webAppAdmin.enroll("webAppAdminSecret",function(err){...}
-	chain.setRegistrarMember(webAppAdmin);
+	chain.setRegistrar(webAppAdmin);
 	chain.getMember("user1",function(err,user) {
 		if (err) return console.log("can't get member: %j",err);
 		console.log("got %s: %s",user.getName(),user);
