78bdb2eedaf93c3aac28d9a13ab2b311d8b50f83
rameshthoomu <rameshbabu.thoomU@gmail.com>
2016-04-20 13:53:57 -0400

    Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if grep -q 'FAIL' "/$HOME/gopath/src/github.com/hyperledger/fabric/build-result.txt"; then
-cat $HOME/gopath/src/github.com/hyperledger/fabric/build-result.txt
+if grep -q 'FAIL' "/$HOME/gopath/src/github.com/hyperledger/fabric/peer/build-result.txt"; then
+cat $HOME/gopath/src/github.com/hyperledger/fabric/peer/build-result.txt
 echo "=========> BUILD FAILED =========="
 else
-cat $HOME/gopath/src/github.com/hyperledger/fabric/build-result.txt
+cat $HOME/gopath/src/github.com/hyperledger/fabric/peer/build-result.txt
 echo "==========> BUILD PASSED ========="
 fi
