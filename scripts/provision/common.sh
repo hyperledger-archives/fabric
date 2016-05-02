83730597b1e76cf1440e7737250ee7f97e9e45f0
Gregory Haskins <gregory.haskins@gmail.com>
2016-05-02 18:50:56 -0400

    Ensure we run our provisioners _after_ the foldercopy occurs under travis
    We also want to ensure we have a proper environment for the host.sh provisioner.
    
    Therefore, we run the scripts without sudo and only use sudo as appropriate
    when required.
    
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- scripts/provision/common.sh
+++ scripts/provision/common.sh
@@ -2,7 +2,7 @@
 
 # Add any logic that is common to all (vagrant, travis, docker, etc) environments here
 
-apt-get update -qq
+sudo apt-get update -qq
 
 # Used by CHAINTOOL
-apt-get install -y default-jre
+sudo apt-get install -y default-jre
