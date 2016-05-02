ec9f0bf736e1e404ad724be3c5c072cf0508856d
Mr. Angry <adderbak@us.ibm.com>
2016-05-02 18:23:32 -0400

    Updating chain-tests.js and README.md
    Signed-off-by: Anna D Derbakova <adderbak@us.ibm.com>

--- sdk/node/test/unit/chain-tests.js
+++ sdk/node/test/unit/chain-tests.js
@@ -1,23 +1,25 @@
 var hlc = require('../../hlc');
 var test = require('tape');
-var tapSpec = require('tap-spec');
 
 //
-//  Make test output pretty
-//
-
-test.createStream()
-  .pipe(tapSpec())
-  .pipe(process.stdout);
-
-//
-//  Configure a test chain
+//  Create a test chain
 //
 
 var chain = hlc.newChain("testChain");
+
+//
+// Configure the test chain
+//
+// Set the directory for the local file-based key value store and point to the
+// address of membership service.
+//
+
 chain.configureKeyValStore({dir:"/tmp/keyValStore"});
 chain.setMemberServicesUrl("grpc://localhost:50051");
 
+//
+// Register the myWebAppAdmin member, which will serve as the chain 'registrar'.
+//
 
 test('Set web app administrator', function(t) {
     t.plan(1);
@@ -26,24 +28,29 @@ test('Set web app administrator', function(t) {
     chain.getMember("myWebAppAdmin", function(err, webAppAdmin) {
     	if (err) {
         t.fail("Failed to get webAppAdmin member: " + err)
+      } else {
+        // Set the webAppAdmin as the designated chain registrar
+        chain.setRegistrar(webAppAdmin);
+
+        // Confirm that the chain registrar is now webAppAdmin
+        t.equal(chain.getRegistrar().getName(), 'myWebAppAdmin', 'Successfully set webAppAdmin!');
       }
-
-      // Set the webAppAdmin as the designated chain registrar
-    	chain.setRegistrar(webAppAdmin);
-
-      // Check that the chain registrar is now webAppAdmin
-      t.equal(chain.getRegistrar().getName(), 'myWebAppAdmin', 'Successfully set webAppAdmin!');
     });
 });
+
+//
+// Register an additional user, 'user1', using the myWebAppAdmin 'registrar'
+// member.
+//
 
 test('Register and enroll user1', function(t) {
   t.plan(1);
 
-  chain.getMember("user1",function(err,user) {
+  chain.getMember("user1", function(err, user) {
     if (err) {
       t.fail("Failed to get user1: " + err);
+    } else {
+      t.pass("Successfully registered and enrolled user1: " + user);
     }
-
-    t.pass("Successfully registered and enrolled user1: " + user);
-    });
+  });
 });
