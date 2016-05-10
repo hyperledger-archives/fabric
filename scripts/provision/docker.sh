b6acb827e3c845e160bc76013d0e89964e0cd4f5
Gregory Haskins <gregory.haskins@gmail.com>
2016-05-10 13:09:58 -0400

    Build docker baseimage on demand
    There may be occasions where certain platforms lag behind others w.r.t.
    publishing docker baseimages to dockerhub.  Today, resolution of this
    condition is at best a manual process.  Now that we have a formal build
    system in place, lets make this a little more intelligent.
    
    What this patch does is it treats dockerhub as an optimization, a form
    of build-caching.  However, if for whatever reason, an artifact is not
    found on dockerhub, we will attempt to build the base image locally.
    
    This is, of course, slower, but it is probably more graceful than
    failing the build outright. 
    
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- scripts/provision/docker.sh
+++ scripts/provision/docker.sh
@@ -49,11 +49,35 @@
 
 NAME=hyperledger/fabric-baseimage
 RELEASE=`uname -m`-$1
-FQN=$NAME:$RELEASE
+DOCKERHUB_NAME=$NAME:$RELEASE
 
 CURDIR=`dirname $0`
 
-docker pull $FQN
+echo "BUILD-CACHE: Pulling \"$DOCKERHUB_NAME\" from dockerhub.."
+docker pull $DOCKERHUB_NAME
+docker inspect $DOCKERHUB_NAME 2>&1 > /dev/null
+if [ "$?" == "0" ]; then
+    echo "BUILD-CACHE: Success!"
+    BASENAME=$DOCKERHUB_NAME
+else
+    echo "BUILD-CACHE: WARNING - Build-cache unavailable, attempting local build"
+    (cd $CURDIR/../../images/base && make docker DOCKER_TAG=localbuild)
+    if [ "$?" != "0" ]; then
+        echo "ERROR: Build-cache could not be compiled locally"
+        exit -1
+    fi
+    BASENAME=$NAME:localbuild
+fi
+
+# Ensure that we have the baseimage we are expecting
+docker inspect $BASENAME 2>&1 > /dev/null
+if [ "$?" != "0" ]; then
+   echo "ERROR: Unable to obtain a baseimage"
+   exit -1
+fi
+
+# any further errors should be fatal
+set -e
 
 TMP=`mktemp -d`
 DOCKERFILE=$TMP/Dockerfile
@@ -66,8 +90,8 @@ cp -R $CURDIR/* $LOCALSCRIPTS
 
 # extract the FQN environment and run our common.sh to create the :latest tag
 cat <<EOF > $DOCKERFILE
-FROM $FQN
-`for i in \`docker run -i $FQN /bin/bash -l -c printenv\`;
+FROM $BASENAME
+`for i in \`docker run -i $BASENAME /bin/bash -l -c printenv\`;
 do
    echo ENV $i
 done`
