3aa1fbcb05b8a2c1fad02b356ac5b6c454009cb8
rameshthoomu <rameshbabu.thoomU@gmail.com>
2016-05-13 11:03:44 -0400

    repository name fix for travis builds

--- scripts/foldercopy.sh
+++ scripts/foldercopy.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+
 if [ "$1" = "false" ] && [ "$2" != "hyperledger" ]; then
+	
+	echo " Pull Request number is $1 "
+	echo " User Name is $2 "
+	echo " Repository Name is $3 "
+
 rm -rf $HOME/gopath/src/github.com/hyperledger/
-echo "Deleted hyperledger folder"
-cp -r $HOME/gopath/src/github.com/$2 $HOME/gopath/src/github.com/hyperledger
-echo "Copied User Directory into hyperledger"
+	
+	echo "creating fabric folder to copy files"
+
+mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
+
+	echo "hyperledger/fabric folder created"
+
+cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
+
+	echo "Copied $2 files into hyperledger/fabric folder"
+
 elif [ "$2" != "hyperledger" ]; then
-mv $HOME/gopath/src/github.com/$2 $HOME/gopath/src/github.com/hyperledger
+
+mkdir -p $HOME/gopath/src/github.com/hyperledger/fabric
+
+	echo "hyperledger/fabric folder created"
+
+cp -r $HOME/gopath/src/github.com/$2/$3/* $HOME/gopath/src/github.com/hyperledger/fabric/
+	
+	echo "copied $2 user repo into hyperledger/fabric folder"
+
 fi
