00e1792531f123ec37a1c012cc81a8b9cbf86fef
manish <manishsethi@in.ibm.com>
2016-05-16 17:33:13 +0530

    Support for empty array values - fixes for state-delta

--- core/ledger/statemgmt/state_delta_test.go
+++ core/ledger/statemgmt/state_delta_test.go
@@ -50,3 +50,29 @@ func TestStateDeltaCryptoHash(t *testing.T) {
 	stateDelta.Delete("chaincodeID2", "key1", nil)
 	testutil.AssertEquals(t, stateDelta.ComputeCryptoHash(), testutil.ComputeCryptoHash([]byte("chaincodeID1key1value1key2value2chaincodeID2key1key2value2")))
 }
+
+func TestStateDeltaEmptyArrayValue(t *testing.T) {
+	stateDelta := NewStateDelta()
+	stateDelta.Set("chaincode1", "key1", []byte("value1"), nil)
+	stateDelta.Set("chaincode2", "key2", []byte{}, nil)
+	stateDelta.Set("chaincode3", "key3", nil, nil)
+	stateDelta.Set("chaincode4", "", []byte("value4"), nil)
+
+	by := stateDelta.Marshal()
+	t.Logf("length of marshalled bytes = [%d]", len(by))
+	stateDelta1 := NewStateDelta()
+	stateDelta1.Unmarshal(by)
+
+	v := stateDelta1.Get("chaincode2", "key2")
+	if v.GetValue() == nil || len(v.GetValue()) > 0 {
+		t.Fatalf("An empty array expected. found = %#v", v)
+	}
+
+	v = stateDelta1.Get("chaincode3", "key3")
+	if v.GetValue() != nil {
+		t.Fatalf("Nil value expected. found = %#v", v)
+	}
+
+	v = stateDelta1.Get("chaincode4", "")
+	testutil.AssertEquals(t, v.GetValue(), []byte("value4"))
+}
