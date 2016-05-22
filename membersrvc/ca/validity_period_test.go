48e1410d49364e1b68a6dde00c1a9a3c50fc7436
JonathanLevi <jonathan@levi.name>
2016-05-22 09:59:12 -0400

    Fixes #1510: lint issues in membersrvc/ca/validity_period_test.go

--- membersrvc/ca/validity_period_test.go
+++ membersrvc/ca/validity_period_test.go
@@ -98,17 +98,17 @@ func TestValidityPeriod(t *testing.T) {
 
 	// 6. Compare the values
 	if validityPeriodA != validityPeriodFromLedgerA {
-		t.Logf("Validity period read from ledger must be equals tothe one obtained by querying the Openchain. Expected: %s, Actual: %s", validityPeriod_A, validityPeriodFromLedger_A)
+		t.Logf("Validity period read from ledger must be equals tothe one obtained by querying the Openchain. Expected: %s, Actual: %s", validityPeriodA, validityPeriodFromLedgerA)
 		t.Fail()
 	}
 
 	if validityPeriodB != validityPeriodFromLedgerB {
-		t.Logf("Validity period read from ledger must be equals tothe one obtained by querying the Openchain. Expected: %s, Actual: %s", validityPeriod_B, validityPeriodFromLedger_B)
+		t.Logf("Validity period read from ledger must be equals tothe one obtained by querying the Openchain. Expected: %s, Actual: %s", validityPeriodB, validityPeriodFromLedgerB)
 		t.Fail()
 	}
 
 	if validityPeriodB-validityPeriodA != updateInterval {
-		t.Logf("Validity period difference must be equal to the update interval. Expected: %s, Actual: %s", updateInterval, validityPeriod_B-validityPeriod_A)
+		t.Logf("Validity period difference must be equal to the update interval. Expected: %s, Actual: %s", updateInterval, validityPeriodB-validityPeriodA)
 		t.Fail()
 	}
 
