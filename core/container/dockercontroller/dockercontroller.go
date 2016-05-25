522365400ccb305cd4951ce89cf8f322fe07d7e7
Baohua Yang <yangbaohua@gmail.com>
2016-05-25 20:42:01 +0800

    Let chaincode container networkmode configurable
    Closes #1582.
    
    Signed-off-by: Baohua Yang <baohyang@cn.ibm.com>

--- core/container/dockercontroller/dockercontroller.go
+++ core/container/dockercontroller/dockercontroller.go
@@ -26,6 +26,7 @@ import (
 	"github.com/hyperledger/fabric/core/container/ccintf"
 	cutil "github.com/hyperledger/fabric/core/container/util"
 	"github.com/op/go-logging"
+	"github.com/spf13/viper"
 	"golang.org/x/net/context"
 )
 
@@ -96,7 +97,7 @@ func (vm *DockerVM) Start(ctxt context.Context, ccid ccintf.CCID, args []string,
 		dockerLogger.Error(fmt.Sprintf("start-could not recreate container %s", err))
 		return err
 	}
-	err = client.StartContainer(containerID, &docker.HostConfig{NetworkMode: "host"})
+	err = client.StartContainer(containerID, &docker.HostConfig{NetworkMode: viper.GetString("core.chaincode.containerNetworkmode")})
 	if err != nil {
 		dockerLogger.Error(fmt.Sprintf("start-could not start container %s", err))
 		return err
