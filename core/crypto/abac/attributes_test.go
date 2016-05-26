59b62d70721712bdcb62c51b89a3934fdb4b49a4
Diego Masini <diego.masini@gmail.com>
2016-05-26 16:08:32 -0700

    Added abac unit tests and resources

--- core/crypto/abac/attributes_test.go
+++ core/crypto/abac/attributes_test.go
@@ -17,13 +17,16 @@ limitations under the License.
 package abac
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
 	"testing"
 
 	"github.com/hyperledger/fabric/core/crypto/primitives"
 )
 
 func TestEncryptDecryptAttributeValuePK0(t *testing.T) {
-	expected := "acme"
+	expected := "ACompany"
 
 	preK0 := []byte{
 		91, 206, 163, 104, 247, 74, 149, 209, 91, 137, 215, 236,
@@ -47,8 +50,52 @@ func TestEncryptDecryptAttributeValuePK0(t *testing.T) {
 		t.Error(err)
 	}
 
-	if string(attribute[:]) != expected {
+	if string(attribute) != expected {
 		t.Errorf("Failed decrypting attribute. Expected: %v, Actual: %v", expected, attribute)
+	}
+}
+
+func TestGetKAndValueForAttribute(t *testing.T) {
+	expected := "ACompany"
+
+	if err := primitives.InitSecurityLevel("SHA2", 256); err != nil {
+		t.Errorf("Failed setting security level: %v", err)
+	}
+
+	tcert, prek0, err := loadTCertAndPreK0()
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, attribute, err := getKAndValueForAttribute("company", prek0, tcert)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(attribute) != expected {
+		t.Errorf("Failed retrieving attribute value from TCert. Expected: %v, Actual: %v", expected, string(attribute))
+	}
+}
+
+func TestGetValueForAttribute(t *testing.T) {
+	expected := "ACompany"
+
+	if err := primitives.InitSecurityLevel("SHA2", 256); err != nil {
+		t.Errorf("Failed setting security level: %v", err)
+	}
+
+	tcert, prek0, err := loadTCertAndPreK0()
+	if err != nil {
+		t.Error(err)
+	}
+
+	value, err := GetValueForAttribute("company", prek0, tcert)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(value) != expected {
+		t.Errorf("Failed retrieving attribute value from TCert. Expected: %v, Actual: %v", expected, string(value))
 	}
 }
 
@@ -78,4 +125,29 @@ func TestBuildAndParseAttributesHeader(t *testing.T) {
 	if components["position"] != 2 {
 		t.Errorf("Error parsing header. Expected %v with value %v, found %v instead", "position", 2, components["position"])
 	}
+}
+
+func loadTCertAndPreK0() (*x509.Certificate, []byte, error) {
+	preKey0, err := ioutil.ReadFile("./test_resources/prek0.dump")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	tcertRaw, err := ioutil.ReadFile("./test_resources/tcert.dump")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	_, tcertPem := pem.Decode(tcertRaw)
+
+	tcert, err := x509.ParseCertificate(tcertPem)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return tcert, preKey0, nil
 }
