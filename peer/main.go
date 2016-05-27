aaba76697d0e8bd305eb1da6aaf77473b3760963
yeasy <yangbaohua@gmail.com>
2016-05-27 10:43:26 +0800

    update []string init

--- peer/main.go
+++ peer/main.go
@@ -156,7 +156,7 @@ var networkLoginCmd = &cobra.Command{
 
 var networkListCmd = &cobra.Command{
 	Use:   "ls",
-	Aliases:   ["list"],
+	Aliases: []string{"list"},
 	Short: "Lists all network peers.",
 	Long:  `Returns a list of all existing network connections for the target peer node, includes both validating and non-validating peers.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
