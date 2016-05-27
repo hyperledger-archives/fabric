e348ba81229c8e0c9482aa8be63049c7252721cd
Baohua Yang <yangbaohua@gmail.com>
2016-05-27 10:00:25 +0800

    Use peer network ls instead of list by default

--- peer/main.go
+++ peer/main.go
@@ -155,7 +155,8 @@ var networkLoginCmd = &cobra.Command{
 // }
 
 var networkListCmd = &cobra.Command{
-	Use:   "list",
+	Use:   "ls",
+	Aliases:   ["list"],
 	Short: "Lists all network peers.",
 	Long:  `Returns a list of all existing network connections for the target peer node, includes both validating and non-validating peers.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
