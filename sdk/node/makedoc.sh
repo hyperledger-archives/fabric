bdd6e23f69b12b54adc32b67d877ba4216fb982d
Keith Smith <bksmith@us.ibm.com>
2016-05-31 09:40:48 -0400

    Convert hlc to typescript.  Enhance README with samples, etc.  Add more tests.

---
+++ sdk/node/makedoc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+typedoc=$(which typedoc)
+if [[ $? -ne 0 ]]; then
+    echo "Installing typedoc ..."
+    sudo npm install -g typedoc
+    if [[ $? -ne 0 ]]; then
+       echo "No typedoc found. Please install it like this:"
+       echo "  npm install -g typedoc"
+       echo "and rerun this shell script again."
+       exit 1
+    fi
+    echo "Successfully installed typedoc"
+fi
+set -e
+
+tv="$(typings -v)"
+if [[ "${tv%.*}" < "1.0" ]]; then
+    echo "You have typings ${tv} but you need 1.0 or higher."
+    exit 1
+fi
+
+mkdir -p tsdoc
+rm -rf tsdoc/*
+
+typedoc -m amd \
+	--name 'Hyperledger OpenBlockChain' \
+	--out tsdoc \
+	typedoc-special.d.ts \
+	src/crypto.ts \
+	src/hlc.ts
