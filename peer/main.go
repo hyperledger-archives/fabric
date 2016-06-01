3a834d8d54b7f4603845249c895349b3d7d0ca18
Baohua Yang <yangbaohua@gmail.com>
2016-06-01 11:41:00 +0800

    fix format problem

--- peer/main.go
+++ peer/main.go
@@ -155,10 +155,10 @@ var networkLoginCmd = &cobra.Command{
 // }
 
 var networkListCmd = &cobra.Command{
-	Use:   "list",
+	Use:     "list",
 	Aliases: []string{"ls"},
-	Short: "Lists all network peers.",
-	Long:  `Returns a list of all existing network connections for the target peer node, includes both validating and non-validating peers.`,
+	Short:   "Lists all network peers.",
+	Long:    `Returns a list of all existing network connections for the target peer node, includes both validating and non-validating peers.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return networkList()
 	},
