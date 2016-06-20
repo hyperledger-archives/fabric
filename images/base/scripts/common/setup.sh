292a70e1b8b56085743058436ef5fb867455aa4b
Srirama Sharma <sriramsh@in.ibm.com>
2016-06-20 08:24:24 -0400

    Support hyperledger fabric build on ppc64le platform

--- images/base/scripts/common/setup.sh
+++ images/base/scripts/common/setup.sh
@@ -16,6 +16,9 @@ apt-get dist-upgrade -qqy
 # install git
 apt-get install --yes git
 
+# install sudo
+apt-get install --yes sudo
+
 # Set Go environment variables needed by other scripts
 export GOPATH="/opt/gopath"
 
@@ -27,10 +30,16 @@ if [ x$MACHINE = xs390x ]
 then
    apt-get install --yes golang
    export GOROOT="/usr/lib/go-1.6"
-elif [ x$MACHINE = xppc64 ]
+elif [ x$MACHINE = xppc64le ]
 then
-   echo "TODO: Add PPC support"
-   exit
+   wget ftp://ftp.unicamp.br/pub/linuxpatch/toolchain/at/ubuntu/dists/trusty/at9.0/binary-ppc64el/advance-toolchain-at9.0-golang_9.0-3_ppc64el.deb
+   dpkg -i advance-toolchain-at9.0-golang_9.0-3_ppc64el.deb
+   rm advance-toolchain-at9.0-golang_9.0-3_ppc64el.deb
+
+   update-alternatives --install /usr/bin/gofmt gofmt /usr/local/go/bin/gofmt 9
+   update-alternatives --install /usr/bin/gofmt gofmt /usr/local/go/bin/gofmt 9
+
+   export GOROOT="/usr/local/go"
 elif [ x$MACHINE = xx86_64 ]
 then
    export GOROOT="/opt/go"
@@ -45,6 +54,9 @@ then
    mv go $GOROOT
    chmod 775 $GOROOT
    rm go$GO_VER.linux-${ARCH}.tar.gz
+else
+  echo "TODO: Add $MACHINE support"
+  exit
 fi
 
 PATH=$GOROOT/bin:$GOPATH/bin:$PATH
@@ -59,6 +71,9 @@ EOF
 # Install NodeJS
 
 if [ x$MACHINE = xs390x ]
+then
+    apt-get install --yes nodejs
+elif [ x$MACHINE = xppc64le ]
 then
     apt-get install --yes nodejs
 else
@@ -123,6 +138,11 @@ then
     echo There were some bugs in 4.1 for x/p, dev stream has the fix, living dangereously, fixing in place
     sed -i -e "s/-march=native/-march=z196/" build_tools/build_detect_platform
     sed -i -e "s/-momit-leaf-frame-pointer/-DDUMBDUMMY/" Makefile
+elif [ x$MACHINE = xppc64le ]
+then
+    echo There were some bugs in 4.1 for x/p, dev stream has the fix, living dangereously, fixing in place.
+    echo Below changes are not required for newer releases of rocksdb. 
+    sed -ibak 's/ifneq ($(MACHINE),ppc64)/ifeq (,$(findstring ppc64,$(MACHINE)))/g' Makefile
 fi
 
 PORTABLE=1 make shared_lib
