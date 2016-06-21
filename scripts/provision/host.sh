ee115f4bcff742a70b3c9aa3a70d699e7041f15a
Jeff Garratt <garratt.jeff@gmail.com>
2016-06-21 14:08:38 -0400

    Moved pip install to main line.

--- scripts/provision/host.sh
+++ scripts/provision/host.sh
@@ -19,11 +19,8 @@ apt-get install --yes python-pip
 pip install behave
 pip install nose
 
-# Python grpc package for behave tests
-pip install 'grpcio==0.13.1'
-
 # updater-server, update-engine, and update-service-common dependencies (for running locally)
-pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3
+pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 grpcio==0.13.1
 
 # install ruby and apiaryio
 #apt-get install --yes ruby ruby-dev gcc
