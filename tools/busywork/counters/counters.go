2aa7acf553ebdb5315bb6aac2fdbcc5d025d2a4a
bcbrock <bcbrock@us.ibm.com>
2016-06-21 16:54:19 -0500

    Busywork: Changes to support latest blockchain semantics
    The definition of correctness for **busywork** tests has been relaxed due to
    the realities of the current PBFT consensus algorithms. Due to state transfer,
    we can no longer assume that each peer has actually executed each
    transaction. Instead, we can only demand that the state of the peer is
    consistent with having executed every transaction. We also now wait until the
    peers reach a consensus on the blockchain height at the end of the run before
    querying for final status, since queries now fail on peers that are in the
    middle of state transfer. Once a couple of outstanding consensus PRs are
    processed we will probably be able to state that the busywork/counters test
    case *stress2b* is working.

--- tools/busywork/counters/counters.go
+++ tools/busywork/counters/counters.go
@@ -233,7 +233,7 @@ func (c *counters) initParms(stub *shim.ChaincodeStub, args []string) (val []byt
 	flags.StringVar(&c.id, "id", "", "A unique identifier; Allows multiple copies of this chaincode to be deployed")
 	loggingLevel := flags.String("loggingLevel", "default", "The logging level of the chaincode logger. Defaults to INFO.")
 	shimLoggingLevel := flags.String("shimLoggingLevel", "default", "The logging level of the chaincode 'shim' interface. Defaults to WARNING.")
-	flags.BoolVar(&c.checkCounters, "checkCounters", true, "Default true; If false, no error/consistency checks are made on counter array states.")
+	flags.BoolVar(&c.checkCounters, "checkCounters", false, "Default false. If true, consistency checks are made on counter states during increment/decrement.")
 	flags.BoolVar(&c.checkStatus, "checkStatus", true, "Default true; If false, no error/consistency checks are made in the 'status' method.")
 	err = flags.Parse(args)
 	if err != nil {
