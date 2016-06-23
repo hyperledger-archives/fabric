a7de910ec2b30d46a431eefff85724e620a06164
Satheesh Kathamuthu <satheesh.ceg@gmail.com>
2016-06-23 18:50:34 +0530

    Removed vagrant check in unit test

--- core/chaincode/platforms/java/test/java_test.go
+++ core/chaincode/platforms/java/test/java_test.go
@@ -32,10 +32,6 @@ func TestMain(m *testing.M) {
 
 func TestJava_BuildImage(t *testing.T) {
 
-	if os.Getenv("VAGRANT") == "" {
-		t.Skip("skipping test; only supported within vagrant")
-	}
-
 	vm, err := container.NewVM()
 	if err != nil {
 		t.Fail()
