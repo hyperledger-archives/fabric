ddfc96a64349931032e1501cf3e8f0bc63b8f4a0
grapebaba <281165273@qq.com>
2016-07-19 16:29:14 +0800

    use pip install docker-compose

--- scripts/provision/host.sh
+++ scripts/provision/host.sh
@@ -4,11 +4,7 @@ CURDIR=`dirname $0`
 
 $CURDIR/common.sh
 
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# Install Python, pip, behave, nose
+# Install Python, pip, behave, nose, docker-compose
 #
 # install python-dev and libyaml-dev to get compiled speedups
 apt-get install --yes python-dev
@@ -18,6 +14,7 @@ apt-get install --yes python-setuptools
 apt-get install --yes python-pip
 pip install behave
 pip install nose
+pip install docker-compose
 
 # updater-server, update-engine, and update-service-common dependencies (for running locally)
 pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3
